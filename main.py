a140cbfecbed7916ea73c75fdb8c4045696c054d
Max Andreasen <maxandreasen02@gmail.com>
2026-05-11 21:34:15 +0200

    importing datasets and models

---
+++ main.py
@@ -0,0 +1,13 @@
+
+from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq
+from datasets import load_dataset
+
+
+processor = AutoProcessor.from_pretrained("KBLab/kb-whisper-large")
+model_kb = AutoModelForSpeechSeq2Seq.from_pretrained("KBLab/kb-whisper-large")
+
+processor = AutoProcessor.from_pretrained("openai/whisper-large")
+model_whisp = AutoModelForSpeechSeq2Seq.from_pretrained("openai/whisper-large")
+
+
+ds = load_dataset("NbAiLab/NST", split="train")
